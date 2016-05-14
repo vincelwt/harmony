428eef191771a1db1100427592f780e93c1d2796
Vincent L <vincent.loewert@gmail.com>
2016-05-14 12:26:08 +0200

    Fix first time opening for sc/local

--- app/js/settingsCtrl.js
+++ app/js/settingsCtrl.js
@@ -74,7 +74,7 @@ angular.module('harmony').controller('SettingsController', function($scope) {
     $scope.resetAll = function() {
       console.log("Reseting all...");
       $scope.data = {};
-      $scope.settings = {backgroundNotify: true, repeat: true, shuffle: false, lastfm: {active: false}, spotify: {active: false}, soundcloud: {active: false}, googlepm : {user: '', passwd: '', active: false}, local: {paths:[], active: false}};
+      $scope.settings = {layout: 'list', backgroundNotify: true, repeat: true, shuffle: false, lastfm: {active: false}, spotify: {active: false}, soundcloud: {active: false}, googlepm : {user: '', passwd: '', active: false}, local: {paths:[], active: false}};
       conf.set('settings', $scope.settings);
       conf.set('data', $scope.data);
     }
