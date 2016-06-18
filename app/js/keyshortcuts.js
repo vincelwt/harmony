5306bc43b760b0231cd8618cf6336d4ad56b4917
Vincent L <vincent.loewert@gmail.com>
2016-06-18 12:15:34 +0200

    Allow arrows keys to go up or down

--- app/js/keyshortcuts.js
+++ app/js/keyshortcuts.js
@@ -2,19 +2,31 @@
 
 Mousetrap.bind('down', function(e) {
   if (g.selected != null && g.selected+1 < trackList.length) {
-    document.querySelectorAll("[index='"+g.selected+"']")[0].classList.remove("selected");
+
+    var el = document.querySelectorAll("[index='"+g.selected+"']")[0];
+    var distance_bottom = window.innerHeight - el.getBoundingClientRect().bottom
+
+    el.classList.remove("selected");
     g.selected++;
     document.querySelectorAll("[index='"+g.selected+"']")[0].classList.add("selected");
-    e.preventDefault();
+
+    if (distance_bottom > 50) e.preventDefault();
+
   }
 });
 
 Mousetrap.bind('up', function(e) {
   if (g.selected != null && g.selected > 0) {
-    document.querySelectorAll("[index='"+g.selected+"']")[0].classList.remove("selected");
+
+    var el = document.querySelectorAll("[index='"+g.selected+"']")[0];
+    var distance_top = el.getBoundingClientRect().top
+
+    el.classList.remove("selected");
     g.selected--;
     document.querySelectorAll("[index='"+g.selected+"']")[0].classList.add("selected");
-    e.preventDefault();
+
+    if (distance_top > 50) e.preventDefault();
+    
   }
 });
 
