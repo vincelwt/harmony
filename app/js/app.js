226b8077d6428fe456a7f844efbd41fe65327f12
Vincent L <vincent.loewert@gmail.com>
2016-07-28 17:22:36 +0200

    Fix trackList not updating on start

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ var Configstore = require('configstore');
 var conf = new Configstore("harmony");
 
 var data = settings = g = coverflowContent = {},
-	coverflowItems = coverflowItemsTmp = [],
+	coverflowItems = coverflowItemsTmp = trackList = [],
 	currentCoverIndex;
 
 console.log("We are on a -"+process.platform+"- system")
