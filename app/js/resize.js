c6aec87c1c5210b85a1ae5a5bf6291d348205f3e
Vincent L <vincent.loewert@gmail.com>
2016-09-22 18:09:26 +0200

    Save sidebar width (close #23)

--- app/js/resize.js
+++ app/js/resize.js
@@ -1,19 +1,24 @@
-
 var resizeHandle = document.getElementsByClassName("vertical-resize")[0];
 var navbar = document.getElementsByClassName("sidebar")[0];
 
+if (settings && settings.navbarWidth) navbar.style.width = settings.navbarWidth;
+
 function resizeNavbar(e) {
-  navbar.style.width = (e.pageX+10)+"px";
-  if (e.pageX+10 < 150) {
+  navbar.style.width = (e.pageX+5)+"px";
+  if (e.pageX+5 < 150) {
   	addClass("sidebar", "small-nav");
   } else {
   	removeClass("sidebar", "small-nav");
   }
+
+  settings.navbarWidth = (e.pageX+5)+"px";
 }
 
 function removeEvents() {
   document.removeEventListener('mousemove', resizeNavbar);
   document.removeEventListener('mouseup', resizeNavbar);
+
+  conf.set('settings', settings);
 }
 
 resizeHandle.addEventListener('mousedown', function() {
